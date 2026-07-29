9b5c4145f445d1b72c62921e0887f1c636d686ad
rixicz <patrik.smidoch@hotmail.com>
2026-07-29 07:37:35 +0200

    set up the files

---
+++ script.js
@@ -0,0 +1,4 @@
+const userInput = parseInt(prompt("Type in the size of the one side of the grid (e.g. input: 64, the grid is gonna be 64x64"))
+if (userInput > 100) {
+    alert("The size of the one side has to be less than 100.")
+}
