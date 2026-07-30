daaf4eec797d61850224dcf7b9b9dbd8de312cff
rixicz <patrik.smidoch@hotmail.com>
2026-07-30 15:57:11 +0200

    done the opacity thing, not RGB, but there is a slight problem with borders

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
-const userInput = parseInt(prompt("Type in the size of the one side of the grid (e.g. input: 64, the grid is gonna be 64x64"))
-if (userInput > 100) {
-    alert("The size of the one side has to be less than 100.")
-}
+const sumbitButton = document.querySelector("button")
+const inputForm = document.getElementById("howManySquares")
+sumbitButton.addEventListener("click", () => {
+    const userInput = inputForm.value
+    if (userInput > 100) {
+        alert("The size of the one side has to be less than 100.")
+
+    } else {
+        const squares = userInput ** 2
+        const gridBox = document.querySelector(".container")
+        if (!(gridBox.childNodes.length === 0))
+            gridBox.innerHTML = ""
+
+        for (let i = 0; i < userInput; i++) {
+            let row = document.createElement("div")
+            row.classList = "row"
+            gridBox.appendChild(row)
+
+            for (let j = 0; j < userInput; j++) {
+                let block = document.createElement("div")
+                block.classList = "square"
+                row.appendChild(block)
+                block.addEventListener("mouseover", () => {
+                    block.style.opacity -= '-0.1'                   
+                })
+            }
+        }
+        
+    }
+
+})
+
